834bab47f1d2fe326ee105f0e637229ea044a164
Preston O'Neal <prestononeal@gmail.com>
2017-11-17 17:38:41 -0800

    Basic front end POC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'addr2line-JS';
+  translateMe = '';
+  translated = '';
+
+  fileChange() {
+    console.log('Uploading');
+  }
+
+  translate() {
+    console.log(`Translating ${this.translateMe}`);
+  }
 }
